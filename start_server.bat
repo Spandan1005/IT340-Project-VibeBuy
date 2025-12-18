c63d4913e7f36ebb308748b6e5dfe15f0e8a0121
Spandan1005 <snp85@njit.edu>
2025-12-17 23:27:52 -0500

    cleaned up some extra unnecesary files [deleted extras]

--- start_server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting VibeBuy Backend Server...
-cd VibeBuy_backend
-if not exist node_modules (
-    echo Installing dependencies...
-    call npm install
-)
-node server.js
-pause
